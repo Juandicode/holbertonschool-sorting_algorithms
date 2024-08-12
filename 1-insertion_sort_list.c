f58ef028443e7efa6261801b1345aab8dbfcc177
“juandicode” <juandiegomhs@gmail.com>
2024-08-12 01:23:35 +0000

    insertion

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,28 @@
+#include "sort.h"
+
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *current, *temp;
+
+	if (!list || !*list || !(*list)->next) return;
+
+	for (current = (*list)->next; current; current = temp)
+	{
+		temp = current->next;
+		while (current->prev && current->n < current->prev->n)
+		{
+			current->prev->next = current->next;
+			if (current->next) current->next->prev = current->prev;
+
+			current->next = current->prev;
+			current->prev = current->next->prev;
+			current->next->prev = current;
+
+			if (current->prev) current->prev->next = current;
+			else *list = current;
+
+			print_list(*list);
+		}
+	}
+}
+
